0e40d052f78081c88d14e4224aa23d96424f852e
Refik SÖYLEMEZ <refiksoylemez@gmail.com>
2020-10-08 08:41:20 +0300

    language

--- apps/item/admin.py
+++ apps/item/admin.py
@@ -1,5 +1,13 @@
 from django.contrib import admin
-from .models import Hizmetler
+from .models import Hizmetler,SunulanHizmetler
+from translations.admin import TranslatableAdmin, TranslationInline
 
-# Register your models here.
-admin.site.register(Hizmetler)
+@admin.register(Hizmetler)
+class HizmetlerAdmin(TranslatableAdmin):
+    inlines = [TranslationInline,]
+    filter_horizontal = ('sunulanHizmet',)
+
+@admin.register(SunulanHizmetler)
+class SunulanHizmetlerAdmin(TranslatableAdmin):
+    inlines = [TranslationInline,]
+
--- apps/item/models.py
+++ apps/item/models.py
@@ -5,13 +5,51 @@ from django.db import models
 from ..common.oneTextField.oneField import OneTextField
 from ..common.mixins.audit import AuditMixin
 from django.contrib.auth.models import Group, User
+from translations.models import Translatable
 
 
-class Hizmetler(OneTextField):
+
+class SunulanHizmetler(Translatable):
+    text = models.CharField(max_length=200, null=True, verbose_name="Başlık")
+
     slug = models.SlugField(unique=True, editable=False)
 
     def __str__(self):
         return (self.text)
+
+
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.text)
+        super(SunulanHizmetler, self).save(*args, **kwargs)
+
+    class Meta:
+        verbose_name = _('Sunulan Hizmetler')
+        verbose_name_plural = _('Sunulan Hizmetler')
+        default_permissions = ()
+        permissions = ((_('liste'), _('Listeleme Yetkisi')),
+                       (_('sil'), _('Silme Yetkisi')),
+                       (_('ekle'), _('Ekleme Yetkisi')),
+                       (_('guncelle'), _('Güncelleme Yetkisi'))
+                       )
+
+
+class Hizmetler(Translatable):
+    text = models.CharField(max_length=200, null=True, verbose_name="Başlık")
+
+    detay = models.TextField(null=True, verbose_name=_('Detay'))
+
+    resim = models.ImageField(('Resim'), upload_to='images/', null=True, )
+    sunulanHizmet = models.ManyToManyField(SunulanHizmetler, blank=True, verbose_name="Sunulan Hizmetler")
+    slug = models.SlugField(unique=True, editable=False)
+
+    def __str__(self):
+        return (self.text)
+
+    @property
+    def resim_url(self):
+        if self.resim and hasattr(self.resim, 'url'):
+            return self.resim.url
 
     def save(self, *args, **kwargs):
         self.slug = slugify(self.text)
