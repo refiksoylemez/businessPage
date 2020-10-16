5e756218bb122fcbc2eeb1da1399b3e31dea7d00
Refik SÖYLEMEZ <refiksoylemez@gmail.com>
2020-10-16 08:28:34 +0300

    live in server

--- business/settings.py
+++ business/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '2!y0w=es6oop#&dn&i=$(6#cgmud(wrmt3od+4))zr!i22ic-z'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
